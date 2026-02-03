d1391db709ebe71628986b4d890c97f602dfedc4
blaktiger522 <mahfoudhiahdi@gmail.com>
2026-02-03 13:18:56 +0100

    Debug and fix preview issue

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ const App = () => (
               <Route path="/" element={<Index />} />
               <Route path="/history" element={<HistoryPage />} />
               <Route path="/settings" element={<SettingsPage />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
